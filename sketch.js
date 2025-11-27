f580cefea5efe44b52ef64bf0c95c4b5b9b5a7bf
Yuhong-Xu <xuyuhong@ocadu.ca>
2025-11-27 17:43:19 -0500

    新的文件上传，试一下存不存在路径问题

--- sketch.js
+++ sketch.js
@@ -7,11 +7,10 @@
 
 let attention = 0;   // Satisfaction level
 let icloud;          // 3D model
-let monster;        // p5 play 的某个多眼monster围着icloud跳出来
 
 function preload() {
   // Make sure the model path is correct
-  icloud = loadModel('/little icloud models/good icloud.obj', true);
+  icloud = loadModel('./little_icloud_models/good_icloud.obj', true);
 }
 
 function setup() {
@@ -31,40 +30,21 @@ function draw() {
   // This adds color to the model according to the angle of the surface
   normalMaterial();
   model(icloud);
-
-  // monster的那个函数call了
-  setup();
 }
 
-
-// 💡提醒：每次抓手、吹气、拉拽时，旁边会有p5 play的monster蹦出来，而且是好多
-function setup() {
-	new Canvas(500, 120);
-
-	monster = new Sprite();
-	monster.diameter = 70;
-	monster.image = 'assets/monster.webp';
-	monster.image.offset.y = 6;
-}
-function update() {
-	clear();
-	monster.debug = mouse.pressing();
-}
 
 // 这里应有个“试着用手凭空抓一下它” 的文字出现在屏幕上
 
 // 【input2】这里应有个ML5的“抓手”的函数可被调用
 
-// 这里应该
-
 
 // 【input3】这里应有个p5.js的“对着话筒吹风”的函数可被调用
 
 
-// 这里应有个“云朵在跳跃“的feedback
+// 【feedback1]这里应有个“云朵在跳跃“的feedback
 
 
-// 这里应有个“云朵旁边会有彩带蹦出来”的feedback
+// 【feedback2】这里应有个“云朵旁边会有彩带蹦出来”的feedback
 
 
 
